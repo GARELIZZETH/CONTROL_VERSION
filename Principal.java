6659de2cd0d12f224568caa0f91ed23803fd5024
Lizzeth Gajardo Rebollo <garelizzeth@gmail.com>
2023-12-11 17:17:16 +0100

    commit2

---
+++ Principal.java
@@ -0,0 +1,14 @@
+public class    Principal {//INICIO CLASE
+    
+    public static void main(String[] args) { //INICIO MAIN
+       
+		  System.out.println("COMMIT 1: LUNES");
+          int[] vector = new  int [10];
+
+          System.out.println("COMMIT 2: MARTES");
+          for(int i = 0; i<vector.length; i++){
+            vector[i] = (int)(Math.random() * 6 + 1);
+          }
+        
+	   
+    }//FINAL MAIN
